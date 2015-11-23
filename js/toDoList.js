1bcf2e5d502463f58117b66fa5481ddc072acb7f
Paul Santos <paulmichael.a.santos@gmail.com>
2015-11-23 17:57:18 -0500

    added comments for clarity

--- js/toDoList.js
+++ js/toDoList.js
@@ -9,6 +9,8 @@ $(document).ready(function(){
     if (!toDoTitle || !toDoText) {
       return;
     };
+
+// create the button and new content here
     var deleteListBtn = $("<button>").attr("type", "button").addClass("btn btn-warning btn-circle pull-right deleteListBtn");
     var deleteGlyph = $("<i>").addClass("glyphicon glyphicon-remove");
     deleteListBtn.append(deleteGlyph);
@@ -18,19 +20,21 @@ $(document).ready(function(){
     var newPanel = $("<div>").addClass("panel panel-primary").hide();
     var newPanelHeading = $("<div>").addClass("panel-heading");
     var panelTitle = $("<h4>").addClass("panel-title pull-left").append(toDoTitle);
-
     var clearfix = $("<div>").addClass("clearfix");
-    newPanelHeading.append(panelTitle).append(deleteListBtn).append(checkBtn).append(clearfix);
 
+// appended the title to panel along with buttons
+    newPanelHeading.append(panelTitle).append(deleteListBtn).append(checkBtn).append(clearfix);
+// create new div for new content here
     var newPanelBody = $("<div>").addClass("panel-body");
     var toDoContent = $("<p>").addClass("toDoContent").append(toDoText);
     newPanelBody.append(toDoContent);
-
+// append the new content to the new panel
     newPanel.append(newPanelHeading).append(newPanelBody);
 
+// append the new panel to the body of to do list panel
     $(".toDoBody").append(newPanel);
     newPanel.fadeIn(1500);
-
+// clear variables
     deleteListBtn = null;
     deleteGlyph = null;
     checkBtn = null;
@@ -41,20 +45,17 @@ $(document).ready(function(){
 
 // complete event to apply strike through
   $(".panel").on("click", ".completedBtn", function(){
-
     $(this).siblings(".panel-title").addClass("strike-through");
     $(this).parent().parent().children(".panel-body").children().addClass("strike-through");
-    $(this).remove();
+    $(this).fadeOut("slow");
   });
 
 // delete event to delete the list
   $(".panel").on("click", ".deleteListBtn", function(){
-
     $(this).parent().parent().fadeTo("slow", 0, function(){
       $(this).slideUp("slow", function(){
         $(this).remove();
-      })
-     
+      });
     });
   });
 
@@ -62,5 +63,4 @@ $(document).ready(function(){
   $("#searchBtn").on("click", function(){
     alert("search");
   });
-
 });
